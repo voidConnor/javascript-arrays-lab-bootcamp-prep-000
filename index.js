7f5ee8e2098e6a541ef3d535d8586114117f14ff
Connor Bennett <cunit638@gmail.com>
2020-03-16 17:27:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
+ function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
