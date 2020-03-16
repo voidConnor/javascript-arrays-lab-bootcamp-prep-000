f9273a39bd2d4d3ca1aa26662014bef8c37967fb
Connor Bennett <cunit638@gmail.com>
2020-03-16 17:09:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-var kittens = ["Milo","Otis","Garfield"] //define your array here
+var kittens.push("Ralph") = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
-kittens = [...kittens, "Ralph"]
